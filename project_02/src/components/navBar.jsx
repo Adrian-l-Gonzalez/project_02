70602255111f3df4dcae2d553181a9970a4084bc
Adrian Gonzalez <adrianleogonza@gmail.com>
2023-04-18 22:32:16 -0300

    Creacion de rama Nav , componente NavBar, Hoja de estilos NavBarStyle

--- project_02/src/components/navBar.jsx
+++ project_02/src/components/navBar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import '../styles/NavBarStyles.css'
 
 
 function NavBar() {
     return (
         <>
+        <nav><h2>Adrian</h2></nav>
+        
         </>
     )
 }
