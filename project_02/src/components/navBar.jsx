6df0c234e58ff5008110aca6205cb6d1042ae5dc
Adrian Gonzalez <adrianleogonza@gmail.com>
2023-04-20 23:41:15 -0300

    componente NavBar y estylos

--- project_02/src/components/navBar.jsx
+++ project_02/src/components/navBar.jsx
@@ -2,15 +2,38 @@ import React from "react";
 import '../styles/NavBarStyles.css'
 
 
+function showMenu() {
+    document.querySelector(".navigation").classList.toggle("active");
+    document.querySelector(".menu").classList.toggle("hide");
+    document.querySelector(".close").classList.toggle("show");
+
+}
+
 function NavBar() {
     return (
-        <>
-        <nav><h2>Adrian</h2></nav>
-        
-        </>
-    )
+
+        <section>
+            <div className="logo">
+                <a href="#">logo</a>
+            </div>
+            <nav className="navigation">
+                <a href="#">Home</a>
+                <a href="#">account</a>
+                <a href="#">About</a>
+                <a href="#">sig in</a>
+            </nav>
+             <div className="toggleBox">
+                <a  onClick={showMenu} href="#">
+                <img src="src\components\icons\menu-hamburguesa.png" alt="" className="menu"/>
+                <img src="src\components\icons\cruz.png" alt="" className="close"/>
+                </a>
+            </div>  
+        </section>
+
+    );
+      
 }
 
 
 
-export default NavBar
+export default NavBar;
